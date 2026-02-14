bea9c6eed8d3c7007fef6c1d9611b1794adaaab0
Estarlin Mercedes <estarlinmercedes123@gmail.com>
2026-02-14 11:42:26 -0400

    correción de síntaxis en el index y realización del script

---
+++ script.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let x = 20;
+let animando = false;
+
+function dibujar(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillstyle = 'green';
+    ctx.fillRect(400,80,60,80);
+
+    ctx.fillStyle = 'blue';
+    ctx.arc(x,120,20,0,Math.PI*2);
+    ctx.fill();
+
+    if (animando && x <380){
+        x += 2;
+        requestAnimationFrame(dibujar);
+    }
+}
+
+function inicial(){
+    animando = true;
+    dibujar();
+}
+
+dibujar();
